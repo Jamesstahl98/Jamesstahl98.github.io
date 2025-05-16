6b4d604079f4bb433ffefb272d5fd46c73829708
Jamesstahl98 <Jamesstahl556@gmail.com>
2025-05-16 14:04:02 +0200

    Add project files.

---
+++ MyPortfolio/Models/Project.cs
@@ -0,0 +1,8 @@
+﻿namespace MyPortfolio.Models;
+
+public class Project
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+}
