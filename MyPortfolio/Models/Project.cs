99db76f0ba6d12202a83fb0bb8eb2c1e14561de0
Jamesstahl98 <Jamesstahl556@gmail.com>
2025-05-16 15:54:51 +0200

    Add Projecs +ProjectCarousel

--- MyPortfolio/Models/Project.cs
+++ MyPortfolio/Models/Project.cs
@@ -5,4 +5,5 @@ public class Project
     public string Title { get; set; }
     public string Description { get; set; }
     public string ImageUrl { get; set; }
+    public string ProjectUrl { get; set; }
 }
---
+++ MyPortfolio/wwwroot/js/scrollCarousel.js
@@ -0,0 +1,19 @@
+﻿window.scrollCarousel = (carouselElement, direction) => {
+    const scrollAmount = 300;
+    if (carouselElement && carouselElement.scrollBy) {
+        carouselElement.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
+    }
+};
+
+window.autoScrollCarousel = (carouselElement) => {
+    if (!carouselElement) return;
+
+    const scrollAmount = 300;
+    const maxScrollLeft = carouselElement.scrollWidth - carouselElement.clientWidth;
+
+    if (carouselElement.scrollLeft + 5 >= maxScrollLeft) {
+        carouselElement.scrollTo({ left: 0, behavior: 'smooth' });
+    } else {
+        carouselElement.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    }
+};
